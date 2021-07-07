32697076453fdd38e16999c90a70623bda311dd4
hyejinkang99 <doghj0105@daum.net>
2021-07-07 17:34:39 +0900

    gitignore 등록

---
+++ src/main/java/com/hyejin/book2/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.hyejin.book2.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing//jpa auditing 어노테이션을 모두 활성화
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
